037ec5d49d8c8492d8ed3c5d0ca0f872cecdaecf
Franklin Garcia <frank.estuard.95@gmail.com>
2026-09-20 11:03:51 -0600

    Actualizar caché v2 del cotizador

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fg-cotizador-v1';
+const CACHE='fg-cotizador-v2';
 const FILES=['./cotizador.html','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('fg-cotizador-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
